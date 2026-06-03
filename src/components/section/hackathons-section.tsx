7714a8d0892bdce3dcf131dc0630ec976fc006ce
Uvini Samarakoon <uviisamarakoon@gmail.com>
2026-06-03 12:40:24 +0530

    Disable hackathons section

--- src/components/section/hackathons-section.tsx
+++ src/components/section/hackathons-section.tsx
@@ -1,5 +1,14 @@
+export default function HackathonsSection() {
+  return null;
+}
+
+
+
+
+
+
 /* eslint-disable @next/next/no-img-element */
-import { Badge } from "@/components/ui/badge";
+/*import { Badge } from "@/components/ui/badge";
 import Link from "next/link";
 import { DATA } from "@/data/resume";
 import { Timeline, TimelineItem, TimelineConnectItem } from "@/components/timeline";
@@ -79,3 +88,4 @@ export default function HackathonsSection() {
     </section>
   );
 }
+*/
