a7bf372789756b4f34bc75bd677fcec0d9fc6a64
Uvini Samarakoon <uviisamarakoon@gmail.com>
2026-06-03 14:16:09 +0530

    Update GitHub link

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -47,7 +47,7 @@ locationLink: "https://maps.google.com/?q=Sri+Lanka",
     social: {
       GitHub: {
         name: "GitHub",
-        url: "https://dub.sh/dillion-github",
+        url: "https://github.com/uvii329",
         icon: Icons.github,
         navbar: true,
       },
